6f27873f3438d7919e823980e021a064ac86ba6d
woobc <longnun@naver.com>
2024-05-29 12:56:14 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import axios from "axios"
+
+createApp(App).mount('#app')
+
+
+axios.defaults.baseURL = "http://localhost:8080/";
+const app = createApp(App);
+app.config.globalProperties.axios = axios;
+//app.use(store).use(router).mount("#app");
+
